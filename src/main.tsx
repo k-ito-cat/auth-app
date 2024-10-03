addbbb2da776da8b20c759a921f9e78198e0fe39
k-ito <kyohy163@gmail.com>
2024-10-03 09:35:42 +0900

    add: zodバリデーションメッセージの日本語化

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { setZodErrorMap } from "./api/libs/zod-ja.ts";
 
 if (import.meta.env.VITE_ENVIRONMENT === "development") {
   const { worker } = await import("./api/mocks/browser");
@@ -9,6 +10,7 @@ if (import.meta.env.VITE_ENVIRONMENT === "development") {
     onUnhandledRequest: "bypass",
   });
 }
+setZodErrorMap();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
